7535f0cfc6ebe11cdf94e03b018917f4df8e5833
ManaiMohamedSelim <manai.mohamed.selim@gmail.com>
2018-03-13 22:46:32 +0100

    init 0

---
+++ src/UtilisateurBundle/Entity/Utilisateur.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Manai
+ * Date: 13/03/2018
+ * Time: 02:41
+ */
+
+namespace UtilisateurBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class Utilisateur extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+}
